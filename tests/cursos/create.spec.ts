cdb5aa17db6364002e88c10a422cada3fabb1272
Emanuel1138 <emanuelrcsousa@gmail.com>
2026-06-01 16:48:42 -0300

    adicionado expect(...).toBeVisible para verificar a mensagem de sucesso

--- tests/cursos/create.spec.ts
+++ tests/cursos/create.spec.ts
@@ -5,11 +5,14 @@ test('cadastrar um novo curso', async ({ page }) => {
     await expect(page).toHaveURL(/dashboard/);
     await page.getByRole('button', { name: 'Turmas' }).click();
     await page.getByRole('link', { name: 'Cursos' }).click();
+
     await expect(page).toHaveURL(/cursos/);
     await page.getByRole('button', { name: 'Adicionar Curso' }).click();
     await page.getByRole('textbox', { name: 'Nome do Curso: *' }).fill('música');
     await page.getByRole('button', { name: 'Nível de Escolaridade' }).click();
     await page.getByRole('option', { name: 'Licenciatura' }).click();
     await page.getByRole('button', { name: 'Salvar' }).click();
+
+    await expect(page.getByText('Curso salvo com sucesso')).toBeVisible();
 });
 
