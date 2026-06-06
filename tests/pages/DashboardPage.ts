6313ecb50165b84741dc278d8c42cc9b53671780
Emanuel1138 <emanuelrcsousa@gmail.com>
2026-06-06 19:59:27 -0300

    refactor(helpers): cria helper para cadastro de áreas

--- tests/pages/DashboardPage.ts
+++ tests/pages/DashboardPage.ts
@@ -14,10 +14,16 @@ export class DashboardPage {
     await this.page.getByRole('link', { name: 'Cursos' }).click();
     await expect(this.page).toHaveURL(/cursos/);
   }
+
   async goToTurmas() {
     await this.page.getByRole('button', { name: 'Turmas' }).click();
     await this.page.getByRole('link', { name: 'Turmas' }).click();
     await expect(this.page).toHaveURL(/turmas/);
   }
 
+  async goToAreas() {
+    await this.page.getByRole('button', { name: 'Disciplinas' }).click();
+    await this.page.getByRole('link', { name: 'Áreas' }).click();
+    await expect(this.page).toHaveURL(/areas/);
+  }
 }
