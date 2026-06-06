afc133fdb66f7e45fe8b298ce888636fea2b6fd6
Emanuel1138 <emanuelrcsousa@gmail.com>
2026-06-06 19:18:11 -0300

    refactor(tests): reorganiza page objects dentro da estrutura de testes

--- tests/setup/auth.setup.ts
+++ tests/setup/auth.setup.ts
@@ -1,5 +1,5 @@
 import { test as setup, expect } from '@playwright/test';
-import { LoginPage } from '../../pages/LoginPage';
+import { LoginPage } from '../pages/LoginPage';
 import { otpGenerate } from '../helpers/otp';
 import dotenv from 'dotenv';
 
