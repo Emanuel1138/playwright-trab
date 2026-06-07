41568a86c94aa48bd19807c83334a5b807dc1de8
Emanuel1138 <emanuelrcsousa@gmail.com>
2026-06-07 15:03:46 -0300

    test(areas): exclui cobertura automatizada de CRUD de áreas

--- tests/specs/area/delete.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Caso feliz - Delete', async ({ page }) => {
-    await page.getByRole('button', { name: 'Excluir' }).click(); 
-    await page.getByRole('button', { name: 'Excluir' }).click(); 
-    await expect( page.getByRole('alert') 
-        .filter({ hasText: 'Área excluída com sucesso' }) ).toBeVisible();
-});
