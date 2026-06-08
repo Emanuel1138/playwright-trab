e56a7f94abd7d3aae0d9b4d844459f327976d189
Emanuel1138 <emanuelrcsousa@gmail.com>
2026-06-08 04:08:45 -0300

    refactor(areas): cria helper e teste de exclusão de área

---
+++ tests/helpers/areas/deletarArea.ts
@@ -0,0 +1,10 @@
+import { Page, expect } from '@playwright/test';
+import { AreasPage } from '../../pages/AreasPage';
+
+export async function deletarArea(page: Page, nomeArea: string) {
+    const areas = new AreasPage(page);
+
+    await areas.goto();
+
+    await areas.excluirArea(nomeArea);
+}
--- tests/specs/areas/area.spec.ts
+++ tests/specs/areas/area.spec.ts
@@ -1,6 +1,7 @@
 import { test } from '@playwright/test';
 import { cadastrarArea } from '../../helpers/areas/cadastrarArea';
 import { editarArea } from '../../helpers/areas/editarArea';
+import { deletarArea } from '../../helpers/areas/deletarArea';
 
 const nomeArea = `area_${Date.now()}`;
 const nomeAreaEditada = nomeArea + '_edited';
@@ -11,4 +12,8 @@ test('cadastrar uma nova área', async ({ page }) => {
 
 test('editar uma área existente', async ({ page }) => {
     await editarArea(page, nomeArea, nomeAreaEditada);
+});
+
+test('excluir uma área existente', async ({ page }) => {
+    await deletarArea(page, nomeAreaEditada);
 });
